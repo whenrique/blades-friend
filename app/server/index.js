3f59a061ebb9adb039f134ffd21e29a53d635dde
Weslley Cavalcante <whenriquerc@gmail.com>
2019-07-08 01:56:40 -0300

    Add body-parser middleware, public path middleware

--- app/server/index.js
+++ app/server/index.js
@@ -1,15 +1,26 @@
+const path = require('path')
 const express = require('express')
 const app = express()
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 const dotenv = require('dotenv')
 dotenv.config()
 
-const api = require('./api')
+const PUBLIC = path.join(__dirname, '..', 'public')
+app.use(express.static(PUBLIC))
 
 app.get('/', (req, res) => {
-  console.log(req.query)
-  return res.send('Esta é a home!')
+  if (req.query.access_token) {
+    return res.sendFile(`${PUBLIC}/signed.html`)
+  }
+  return res.sendFile(`${PUBLIC}/signin.html`)
 })
 
+const api = require('./api')
 app.use('/api', api)
+
 
 app.listen(process.env.PORT ? process.env.PORT : 9000)
