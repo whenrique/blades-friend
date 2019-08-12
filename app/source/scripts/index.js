b7f3f2428efa2fb0f41c98238d44cabfb53cf576
Weslley Henrique Cavalcante <whenriquerc@gmail.com>
2019-08-12 00:33:55 -0300

    Update index.js

--- app/source/scripts/index.js
+++ app/source/scripts/index.js
@@ -1,15 +1,35 @@
+n/* eslint-disable no-console */
 import 'style/main.scss'
 
 import user from './user'
+import search from './search'
+
+const signedWrapper = document.querySelector('.signed')
+const avatarDefault = 'https://cdn1.iconfinder.com/data/icons/avatars-55/100/avatar_profile_user_music_headphones_shirt_cool-512.png'
+
 user.then(response => {
   const data = response.data.body
-  console.log(data)
-  const name = data.display_name
-  const photo = data.images[0].url
+  const name = data.display_name ? data.display_name : data.email.split('@')[0]
+  const photo = data.images[0] ? data.images[0].url : avatarDefault
 
-  document.querySelector('.signed').style.backgroundImage = photo
-
-  document.querySelector('.signed__photo-profile').src = photo
-  document.querySelector('.signed__photo-profile').alt = name
-  document.querySelector('.signed__name').innerHTML = name.split(' ').shift()
+  signedWrapper.querySelector('.signed__photo-profile').src = photo
+  signedWrapper.querySelector('.signed__photo-profile').alt = name
+  signedWrapper.querySelector('.signed__name').innerHTML = name.split(' ').shift()
 }).catch(err => console.log(err))
+
+const acceptButton = signedWrapper.querySelector('.signed__button-accept')
+acceptButton.addEventListener('click', () => {
+  const searchFieldTemplate = `
+    <div class="tracks">
+      <input type=search class="signed__field field__primary field__search" placeholder="Search music" />
+      <ul class="tracks__list"></ul>
+    </div>
+    `
+  acceptButton.style.display = 'none'
+
+  signedWrapper.classList.add('search-opened')
+  signedWrapper.insertAdjacentHTML('beforeend', searchFieldTemplate)
+
+  const searchfield = signedWrapper.querySelector('.field__search')
+  search(searchfield)
+})
